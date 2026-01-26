108b74e069a8414d6b875ac28e6be1355b3662cc
Shreyeah03 <shreyakhacharia305@gmail.com>
2026-01-26 09:36:01 +0530

    refactor: move socket to app root

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,21 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import Chat from "./pages/Chat";
+
+const socket = io("http://localhost:5000");
+
 function App() {
-  return <Chat />;
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Socket connected:", socket.id);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return <Chat socket={socket} />;
 }
+
+export default App;
